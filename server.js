2931954923f11f29e4013190175ad0eb2cd225d9
moneyhub441 <hubmoney441@gmail.com>
2026-07-31 17:21:17 +0530

    Fix server start for vercel

--- server.js
+++ server.js
@@ -594,21 +594,27 @@ app.use((req, res) => {
 const PORT =
   process.env.PORT || 5000;
 
+
 const startServer = async () => {
+
   await connectDB();
 
   app.listen(PORT, () => {
+
     console.log(
       `Money Hub server running on http://localhost:${PORT}`
     );
+
   });
+
 };
 
 
 if (require.main === module) {
+
   startServer();
-} else {
-  connectDB();
+
 }
+
 
 module.exports = app;
