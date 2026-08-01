44bbb198336c86f35a967de0062c9c7311b99b38
moneyhub441 <hubmoney441@gmail.com>
2026-08-01 11:38:42 +0530

    Fix vercel mongo connection wait

--- api/index.js
+++ api/index.js
@@ -1,3 +1,31 @@
 const app = require("../server");
+const mongoose = require("mongoose");
 
-module.exports = app;
+module.exports = async (req, res) => {
+  try {
+
+    if (mongoose.connection.readyState !== 1) {
+      await mongoose.connect(
+        process.env.MONGO_URI,
+        {
+          serverSelectionTimeoutMS: 30000,
+        }
+      );
+    }
+
+    return app(req, res);
+
+  } catch (error) {
+
+    console.error(
+      "Mongo connection error:",
+      error.message
+    );
+
+    return res.status(500).json({
+      success:false,
+      message:"Database connection failed",
+    });
+
+  }
+};
